fec875f16893e5c6f332dfc66de63f5837671282
Bas de Greeuw <bdegreeuw@ziggo.nl>
2020-04-09 14:59:17 +0200

    Simple prototype

---
+++ docs/main.js
@@ -0,0 +1,63 @@
+
+(function(l, r) { if (l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (window.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.head.appendChild(r) })(window.document);
+(function () {
+    'use strict';
+
+    init();
+
+    function init () {
+        const audio = new Audio('/readSimpleGraph.wav');
+        const canvas = document.querySelector('canvas');
+        draw(canvas);
+        addGraphExplanation(canvas, audio);
+        window.addEventListener('resize', draw(canvas));
+    }
+
+    function addGraphExplanation (canvas, audio) {
+        canvas.addEventListener('click', () => {
+            readGraph(audio);
+        });
+        canvas.addEventListener('touchstart', ()=> {
+            readGraph(audio);
+        });
+    }
+
+    function draw(canvas) {
+        var ctx = (canvas.getContext('2d'));
+        ctx.canvas.width  = window.innerWidth;
+        ctx.canvas.height = window.innerHeight;
+        ctx.beginPath();
+        ctx.moveTo(0, ctx.canvas.height);
+        ctx.lineTo(ctx.canvas.width, 0);
+        ctx.lineWidth = 30;
+        ctx.strokeStyle = "white";
+        ctx.stroke();
+        ctx.closePath();
+        for (let i = 0; i < 10; i++) {
+            const yIncrement = ctx.canvas.height / 10 * i;
+            const xIncrement = ctx.canvas.width / 10 * i;
+            if (i > 0) {
+                ctx.beginPath ();
+                ctx.moveTo ( 0, yIncrement );
+                ctx.lineTo ( 40, yIncrement );
+                ctx.lineWidth = 30;
+                ctx.strokeStyle = "white";
+                ctx.stroke ();
+                ctx.closePath ();
+                ctx.beginPath ();
+                ctx.moveTo ( xIncrement, ctx.canvas.height );
+                ctx.lineTo ( xIncrement, ctx.canvas.height - 40 );
+                ctx.lineWidth = 30;
+                ctx.strokeStyle = "white";
+                ctx.stroke ();
+                ctx.closePath ();
+            }
+        }
+    }
+
+    function readGraph (audio) {
+        audio.currentTime = 0;
+        audio.play();
+    }
+
+}());
---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import serve from 'rollup-plugin-serve'
+import livereload from 'rollup-plugin-livereload'
+
+
+export default {
+    input: 'src/main.js',
+    output: {
+        file: 'docs/main.js',
+        format: 'iife',
+        name: 'bundle',
+    },
+    plugins: [
+        resolve ( {
+            main: true,
+            browser: true
+        } ),
+        commonjs (),
+        serve('docs'),
+        livereload()
+    ]
+}
